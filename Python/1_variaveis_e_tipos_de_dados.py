73c53552db7aa7aad1adf11c310032fcb3263684
Fabri <ism.fabri@gmail.com>
2025-01-31 20:31:40 -0300

    revisando a base

---
+++ Python/1_variaveis_e_tipos_de_dados.py
@@ -0,0 +1,22 @@
+# Variáveis e Tipos de Dados
+
+# -> Receber dados
+# -> Armazenar dados
+# -> Processar dados
+# -> Mostrar dados processados
+
+# -> int (inteiro) 12, -12, 0...
+# -> str (string) - cadeia de caracteres 'Fabri' ou "Fabri", '10' ou "10"...
+# -> float (número de ponto flutuante) 12.7, -12.7...
+# -> bool (booleano) - True ou False
+
+idade = 21 # variável e int
+IDADE = '19' # constante e string
+f_idade = 21.2 # float
+b_idade = True # bool
+
+print(idade)
+print(type(idade))
+print(type(IDADE))
+print(type(f_idade))
+print(type(b_idade))
