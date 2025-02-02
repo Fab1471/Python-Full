efc101fee2c402faf68f5173d613d33e71794a30
Fabri <ism.fabri@gmail.com>
2025-02-02 17:00:50 -0300

    update 6 and 7

---
+++ Python/6_operadores_relacionais.py
@@ -0,0 +1,12 @@
+# Operador Relacional é um Operador Booleano que retorna True ou False
+
+# == igual
+# != diferente
+# > maior
+# < menor
+# >= maior ou igual
+# <= menor ou igual
+
+operador_relacional = 2 == 2.0
+
+print(operador_relacional)
---
+++ Python/7_operadores_logicos.py
@@ -0,0 +1,7 @@
+# and = os dois precisam ser True para printar True
+# or = pelo menos um precisa ser True para printar True
+# not = inverte o resultado
+
+operador = not (5 == 7 or 3 > 2) and ( 2 == 2 or 5 < 5)
+
+print(operador)
