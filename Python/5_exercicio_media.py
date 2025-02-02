30d3e48900db0247c6508043d5e040e42d442c0f
Fabri <ism.fabri@gmail.com>
2025-02-02 16:25:03 -0300

    update feb 2nd

---
+++ Python/5_exercicio_media.py
@@ -0,0 +1,8 @@
+# Escreva um programa onde o usuário digite duas notas e ele mostre a média das duas notas.
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+
+resultado = (n1 + n2) / 2
+
+print(f'A média das notas {n1} e {n2} é {resultado}')
